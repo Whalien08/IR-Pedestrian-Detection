65d9b15cfb070556701d4d84a42c6d21aa58a043
Whalien08 <mithraxavier2005@gmail.com>
2026-02-21 13:24:12 +0530

    inference

--- gan/src/inference.py
+++ gan/src/inference.py
@@ -2,23 +2,22 @@ import os
 import cv2
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 # 1. Load the model
-model_path = '/kaggle/input/notebooks/himawariricttoslock/gan-4-3/saved_models/thermal_gen_best.h5'
+model_path = r'C:\Users\Nyx\Desktop\IR Pedestrian Detection\gan\final weights\thermal_gan_best.h5'
 model = tf.keras.models.load_model(model_path, compile=False)
 
-def display_thermal_results(input_rgb_folder, num_to_show=8):
-    img_list = [f for f in os.listdir(input_rgb_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
-    img_list = img_list[:num_to_show] # Limit the number of images displayed
-    
-    # Calculate grid size 
-    cols = 2 
-    rows = (len(img_list) + 1) // 2
-    plt.figure(figsize=(15, rows * 5))
+def save_thermal_results(input_rgb_folder, output_ir_folder):
+    # Ensure output directory exists
+    if not os.path.exists(output_ir_folder):
+        os.makedirs(output_ir_folder)
 
-    for i, img_name in enumerate(tqdm(img_list)):
+    img_list = [f for f in os.listdir(input_rgb_folder) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    
+    print(f"Processing {len(img_list)} images...")
+
+    for img_name in tqdm(img_list):
         path = os.path.join(input_rgb_folder, img_name)
         img = cv2.imread(path)
         if img is None: continue
@@ -40,15 +39,15 @@ def display_thermal_results(input_rgb_folder, num_to_show=8):
         threshold = 100
         thermal_data[thermal_data < threshold] = np.clip(thermal_data[thermal_data < threshold] * 0.2, 0, 255)
 
-        # Plotting
-        plt.subplot(rows, cols, i + 1)
-        plt.imshow(thermal_data, cmap='grey') # 'inferno' or 'magma' gives a realistic thermal look
-        plt.title(f"Thermal: {img_name}")
-        plt.axis('off')
+        # --- SAVE IMAGE LOGIC ---
+        save_path = os.path.join(output_ir_folder, f"ir_{img_name}")
+        cv2.imwrite(save_path, thermal_data)
 
-    plt.tight_layout()
-    plt.show()
+    print(f"\nDone! All generated IR images saved to: {output_ir_folder}")
 
-# Run it to show the first 6 images
-display_thermal_results('/kaggle/input/inputttttttttttttttttttttt/', num_to_show=9)
- 
+# Set your paths
+input_dir = r'C:\Users\Nyx\Desktop\IR Pedestrian Detection\gan\data\generate\rgb images'
+output_dir = r'C:\Users\Nyx\Desktop\IR Pedestrian Detection\gan\data\generate\output ir'
+
+# Run it
+save_thermal_results(input_dir, output_dir)
